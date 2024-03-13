12fad5d0358db82a38795b0f37169930c8a163b7
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-12 22:55:53 -0400

    🌈Styles to use in the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors")
+
+
 export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.yellow,
+      }
+    },
   },
   plugins: [],
 }
