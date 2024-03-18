d3774dc363224814572b866b52379576584500c0
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-17 22:43:25 -0400

    Add MutableRefObject type

--- src/hooks/useCountDownTimer.ts
+++ src/hooks/useCountDownTimer.ts
@@ -1,36 +1,36 @@
-import { useCallback, useState, useRef, useEffect } from 'react'
+import { useCallback, useState, useRef, useEffect, type MutableRefObject } from 'react'
 
 function useCountdown (seconds: number) {
   const [timeLeft, setTimeLeft] = useState(seconds)
-  const intervalRef = useRef<NodeJS.Timer | null>(null)
+  const intervalRef = useRef<MutableRefObject<NodeJS.Timer | null>>({ current: null })
   const hasTimerEnded = timeLeft <= 0
-  const isRunning = intervalRef.current != null
+  const isRunning = intervalRef.current.current != null
 
   const startCountdown = useCallback(() => {
     if (!hasTimerEnded && !isRunning) {
-      intervalRef.current = setInterval(() => {
+      intervalRef.current.current = setInterval(() => {
         setTimeLeft((prevTimeLeft) => prevTimeLeft - 1)
       }, 1000)
     }
   }, [setTimeLeft, hasTimerEnded, isRunning])
 
   const resetCountdown = useCallback(() => {
-    clearInterval(intervalRef.current!)
-    intervalRef.current = null
+    clearInterval(intervalRef.current.current!)
+    intervalRef.current.current = null
     setTimeLeft(seconds)
   }, [seconds])
 
   // when the countdown reaches 0, clear the countdown interval
   useEffect(() => {
     if (hasTimerEnded) {
-      clearInterval(intervalRef.current!)
-      intervalRef.current = null
+      clearInterval(intervalRef.current.current!)
+      intervalRef.current.current = null
     }
   }, [hasTimerEnded])
 
   // clear interval when component unmounts
   useEffect(() => {
-    return () => { clearInterval(intervalRef.current!) }
+    return () => { clearInterval(intervalRef.current.current!) }
   }, [])
 
   return { timeLeft, startCountdown, resetCountdown }
